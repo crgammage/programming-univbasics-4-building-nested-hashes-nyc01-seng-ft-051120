bee2b4cfd21a826a8960ff25c9505ac11041721c
Christa Gammage <crgammage@gmail.com>
2020-04-23 15:36:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,6 @@
 def base_hash
   railroads = {}
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-
+  railroads
 end
 
 def monopoly_with_second_tier
