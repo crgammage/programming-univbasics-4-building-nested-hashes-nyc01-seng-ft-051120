8794d6005c9d5b34260c626cdc3abc9710bd638a
Christa Gammage <crgammage@gmail.com>
2020-04-23 15:39:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,16 +9,9 @@ end
 def monopoly_with_second_tier
   railroads = {
     :railroads => {
-      :pieces => 4
-      :rent_in_dollars => {
 
-      }
-      :name => {
-
-      }
     }
   }
-
 end
 
 def monopoly_with_third_tier
